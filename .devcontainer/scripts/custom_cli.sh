0a34904945f2ad0f9ca1aff759b52f26c548c140
Jakub <jakubtolsciuk@gmail.com>
2023-01-07 13:26:10 +0100

    Add custom cli - devcontainer (#5)

---
+++ .devcontainer/scripts/custom_cli.sh
@@ -0,0 +1,13 @@
+
+# * Show git branch name and make a new line
+force_color_prompt=yes
+color_prompt=yes
+parse_git_branch() {
+ git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+if [ "$color_prompt" = yes ]; then
+ PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(parse_git_branch)\[\033[00m\]\n\$ '
+else
+ PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w$(parse_git_branch)\n\$ '
+fi
+unset color_prompt force_color_prompt
