2e919a1be83e954da764d4e5ce4b0eed9dfee531
Jakub <jakubtolsciuk@gmail.com>
2023-01-08 22:30:29 +0000

    Update path

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from loaders import ConfigLoader
 
 def main() -> None:
     print("Hello python!")
-    print(ConfigLoader.load(config_file_path="./config.yaml"))
+    print(ConfigLoader.load(config_file_path="./config_example.yaml"))
 
 
 if __name__ == "__main__":
