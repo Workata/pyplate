91b4f49b040c450b1e5a4dc99a2ff8c440552fd8
Jakub <jakubtolsciuk@gmail.com>
2023-01-22 12:32:26 +0000

    Add setup py, update readme

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='basic-python-project-template',
+    version='0.0.1',
+    author='Jakub Tolsciuk',
+    description='This is a template project',
+    author_email='jakubtolsciuk@gmail.com',
+    url='https://github.com/Workata/basic-python-project-template',
+    keywords='template, example',
+    python_requires='>=3.11, <4',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
