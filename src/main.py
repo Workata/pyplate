3cec97c33ccc30d588a51f9c2f8f74b27febb53a
Jakub <jakubtolsciuk@gmail.com>
2023-02-06 00:04:12 +0100

    Add jupyter notebook setup (#11)

--- src/main.py
+++ src/main.py
@@ -1,13 +1,5 @@
-from loaders import ConfigLoader
-from utils import Logger
-
-
 def main() -> None:
-    logger = Logger(logs_location="default")
-    logger.info("Hello python!")
-
     print("Hello python!")
-    print(ConfigLoader.load(config_file_path="./config_example.yaml"))
 
 
 if __name__ == "__main__":
