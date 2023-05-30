82ff20f3a21cb9a9000edc879828dc0d7128a35f
Jakub <jakubtolsciuk@gmail.com>
2023-05-30 11:13:31 +0200

    Add coverage (#16)
    * Add coverage
    
    * Fixes

--- scripts/run_checks.sh
+++ scripts/run_checks.sh
@@ -1,8 +1,14 @@
 echo "Check typing with mypy..."
 mypy ./src/
+
 echo "Format with Black..."
 black ./src/
+
 echo "Lint with flake8..."
 flake8 ./src/
-echo "Test with PyTest..."
-pytest ./src/tests/
+
+echo "Test with PyTest... (+ check coverage)"
+coverage run -m pytest ./src/
+
+echo "Report coverage..."
+coverage report -m
