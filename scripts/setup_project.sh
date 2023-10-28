5fd82e77c75869aacc8078eb24f950a4eefbc569
workata <jakubtolsciuk@gmail.com>
2023-10-28 19:09:59 +0200

    update

--- scripts/setup_project.sh
+++ scripts/setup_project.sh
@@ -5,12 +5,17 @@ read -p "[Optional] Enter short description: " description
 echo "# ${project_name}" > README.md
 
 find . -type f -not \(      \
-    -path "./venv/*" -o     \
-    -path "./.git/*" -o     \
-    -path "./imgs/*" -o     \
-    -path "./scripts/*" -o  \
-    -path "./logs/*"   -o  \
+    -path "./venv/*" -o        \
+    -path "./.git/*" -o        \
+    -path "./imgs/*" -o        \
+    -path "./scripts/*" -o     \
+    -path "./logs/*"   -o      \
     -path "./requirements/*"   \
 \) -exec sed -i "s/app-name/${project_name}/g" {} \;
+
+find . -type f -path "./setup.py" -exec sed -i "s/__author__/${author}/g" {} \;
+find . -type f -path "./setup.py" -exec sed -i "s/__description__/${description}/g" {} \;
+
+rm -rf imgs/*
 
 echo "Setup is done!"
