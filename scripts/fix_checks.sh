df9847761d6cd1ae364810706a1ed2dc987273c3
jakub tol <jakubtolsciuk@gmail.com>
2024-01-21 01:37:00 +0100

    test

---
+++ scripts/fix_checks.sh
@@ -0,0 +1,16 @@
+# run checks and potentialy fix them
+
+echo "Check typing with mypy..."
+mypy ./src/
+
+echo "Format with Black..."
+black ./src/
+
+echo "Lint with flake8..."
+flake8 ./src/
+
+echo "Test with PyTest... (+ check coverage)"
+coverage run -m pytest ./src/
+
+echo "Report coverage..."
+coverage report -m
