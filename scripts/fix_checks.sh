f9d5f3ee70f71b11737485f33d2227c10d5cb08c
jakub tol <jakubtolsciuk@gmail.com>
2024-01-22 21:53:35 +0100

    fix mypy

--- scripts/fix_checks.sh
+++ scripts/fix_checks.sh
@@ -1,7 +1,13 @@
 # run checks and potentialy fix them
 
-echo "Run pre-commit checks"
-pre-commit run --all-files
+echo "Check typing with mypy..."
+mypy ./src/
+
+echo "Format with Black..."
+black ./src/
+
+echo "Lint with flake8..."
+flake8 ./src/
 
 echo "Test with PyTest... (+ check coverage)"
 coverage run -m pytest ./src/
